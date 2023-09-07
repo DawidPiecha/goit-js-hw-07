b0f01f16eec66b305a63eb7ac5b7081b8928b424
Dawid <dawidpiecha88@gmail.com>
2023-09-07 21:05:51 +0200

    added basiclightbox stylesheet link and script to 01-gallery html and 02-lightbox html. task 01-gallery using JS - done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,37 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+const galleryElements = galleryItems
+  .map(
+    (element) => `<li class="gallery__item">
+  <a class="gallery__link" href="${element.original}">
+  <img class="gallery__image" alt="${element.description}" src="${element.preview}"  data-source="${element.original}">
+  </a>
+  </li>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", galleryElements);
+
+gallery.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const selectedImage = event.target.dataset.source;
+  const instance = basicLightbox.create(`<img src="${selectedImage}">`);
+
+  instance.show();
+
+  document.addEventListener("keydown", (event) => {
+    event.preventDefault();
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  });
+});
