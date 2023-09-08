ada87b039ad4b51bc5aa8c60e4e472ccc836a1fc
Dawid <dawidpiecha88@gmail.com>
2023-09-08 19:59:41 +0200

    replaced links in 02-lightbox html, 02-lightbox js finished

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+const galleryElements = galleryItems
+  .map(
+    (element) => `<li class="gallery__item">
+  <a class="gallery__link" href="${element.original}">
+  <img class="gallery__image" alt="${element.description}" src="${element.preview}" title="${element.description}">
+  </a>
+  </li>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", galleryElements);
+
+gallery.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const description = event.target.title;
+  const options = {
+    captionData: description,
+    captionDelay: 250,
+  };
+  const lightbox = new SimpleLightbox(".gallery__item a", options);
+});
